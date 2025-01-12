70e6e3bc27c031fe17a17899101d53032f7ce5a7
rachelqrwei <rachelqingranwei@gmail.com>
2025-01-12 01:31:03 -0500

    add video ui

--- frontend/src/BloomScroll.jsx
+++ frontend/src/BloomScroll.jsx
@@ -69,8 +69,21 @@ function BloomScroll() {
                         onClick={handlePlayPause}
                         className="absolute bottom-16 left-4 bg-black/50 p-3 rounded-full text-xl w-12 h-12 flex items-center justify-center"
                     >
-                         {isPlaying ? <Pause className="w-6 h-6 text-white" /> : <Play className="w-6 h-6 text-white" />}
+                        {isPlaying ? <Pause className="w-6 h-6 text-white" /> : <Play className="w-6 h-6 text-white" />}
                     </button>
+
+                    <div className="fixed z-100 top-[45%] right-[5%]">
+                        <div className="items-center justify-center text-center pb-5 ">
+                            <Heart className='w-10 h-10 text-white p-1 ' />
+                            <div className='text-white text-xs font-bold'> Like</div>
+                        </div>
+                        <div className="items-center justify-center text-center">
+                            <Share className='w-10 h-10 text-white p-1' />
+                            <div className='text-white text-xs font-bold'> Share</div>
+                        </div>
+                    </div>
+
+
                 </div>
             )}
         </div>
